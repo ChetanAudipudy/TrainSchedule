917036b45d55fa9b2ed53cd3adbefa1571f84e90
Chetan Audipudy <chetan.audipudy@gmail.com>
2018-01-27 14:27:03 -0600

    final with delete button

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -24,13 +24,16 @@ $("#submitBtn").on("click", function(event) {
   var startTime = $("#start-time").val().trim();
   var freq = $("#freq").val().trim();
 
+  var idRef = database.ref().push();
+
 
   database.ref().push({
     trainName: trainName,
     dest: dest,
     startTime: startTime,
     freq: freq,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
+    dateAdded: firebase.database.ServerValue.TIMESTAMP,
+    id: idRef.key
   });
 });
 
@@ -41,6 +44,8 @@ database.ref().on("child_added", function(childSnapshot) {
  var startTime = childSnapshot.val().startTime;    
  var freq = childSnapshot.val().freq;
 
+ var assetKey = childSnapshot.val().id;
+
 //Calculations for the next train to arrive and minutes left
 
 var diffTime = moment().diff(moment(startTime, "hh:mm"), "minutes");
@@ -50,9 +55,11 @@ var minAway = moment().add(nextTrain, "minutes");
 
 //Creating table rows
 
-$("#trainTableBody").append("<tr><td>" + trainName + "</td><td>" + dest + "</td><td>" +
-  freq + "</td><td>" +  minAway.format("hh:mm A") + "</td><td>" + nextTrain + "</td> " + "<td><button class='editbtn'>Edit</button><button class='delbtn'>Delete</button></td></tr>");
+// $("#trainTableBody").append("<tr id=" + assetKey+ "><td>" + trainName + "</td><td>" + dest + "</td><td>" +
+//   freq + "</td><td>" +  minAway.format("hh:mm A") + "</td><td>" + nextTrain + "</td> " + "<td><button class='editbtn'>Edit</button><button class='delbtn'>Delete</button></td></tr>");
 
+$("#trainTableBody").append("<tr id=" + childSnapshot.key + "><td>" + trainName + "</td><td>" + dest + "</td><td>" +
+    freq + "</td><td>" +  minAway.format("hh:mm A") + "</td><td>" + nextTrain + "</td> " + "<td><button class='editbtn'>Edit</button><button class='delbtn'>Delete</button></td></tr>");
   // Handle the errors
 }, function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
@@ -76,12 +83,11 @@ $(document).on("click",'.editbtn', function () {
 
 });
 
-$('#trainTableBody').on('click', ".delbtn", function(){
+$(document).on('click', ".delbtn", function(){
 
-  $(this).closest ('tr').remove();
-
-  var dateAdded = $row.data('dateAdded');
-  database.ref().child(dateAdded).remove();
- 
+  var $row = $(this).closest('tr');
+  var id = $row.attr('id');
+  console.log(id);
+  database.ref().child(id).remove();
+  $row.remove();
 });
-
